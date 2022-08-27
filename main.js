2ea72eddbf6862609fb26de0756ea8e452118575
Arshvir Singh <arshvirsignh16@gmail.com>
2022-08-26 22:02:34 -0600

    Add divs using js.

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const bodyWrapper = document.querySelector("#bodyWrapper");
+
+let grid = document.createElement('div');
+bodyWrapper.appendChild(grid);
+
+for(i = 0; i < 16; i++) {
+    let gridDiv = document.createElement('div');
+    grid.appendChild(gridDiv);
+}
+
+
+
