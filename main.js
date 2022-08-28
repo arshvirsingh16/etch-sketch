3c52ea6a8cac3318c640534e01429df5edca2068
Arshvir Singh <arshvirsignh16@gmail.com>
2022-08-27 21:54:04 -0600

    Add a color option and default option.

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 //Temporary Constants
 const tColor = "black";
 
+//Variables
+let currentButton = 'default';
+let defaultColor = 'white';
+let chosenColor = '';
 
 //CONSTANTS
 const bodyWrapper = document.querySelector("#bodyWrapper");
 const gridMain = document.querySelector("#gridMain");
-const gridPieces = document.querySelectorAll("#gridPiece"); 
+const gridPieces = document.querySelectorAll("#gridPiece");
+const colorButton = document.querySelector("#pen-color"); 
+const eraserButton = document.querySelector("#eraser");
+const gridButton = document.querySelector("#grid-size");
+const clearButton = document.querySelector("clear");
 
 //GRID
 for(i = 0; i < 16; i++){
@@ -14,6 +22,28 @@ for(i = 0; i < 16; i++){
     gridPiece.addEventListener('mouseover', colorGridPiece);
     gridPiece.addEventListener('mousedown', colorGridPiece);
     gridMain.appendChild(gridPiece);
+}
+
+//Buttons Functions
+colorButton.addEventListener('click', pickColor);
+function pickColor() {
+    currentButton = 'bColor';
+    let color = window.prompt("Which color?");
+    let newColor = color.toLowerCase();
+    console.log(newColor);
+    chosenColor = newColor;
+}
+
+eraserButton.addEventListener('click', eraseColor);
+function eraseColor() {
+    currentButton = 'eColor';
+    chosenColor = 'white';
+    console.log(currentButton);
+}
+
+clearButton.addEventListener('click', clearAll);
+function clearAll() {
+    gridPieces.target.style.backgroundColor = "yellow";
 }
 
 
@@ -25,10 +55,22 @@ function colorGridPiece(e) {
         return;
     }
 
-    if(tColor == 'black') {
-        e.target.style.backgroundColor = "black";
+    else if(currentButton == 'default'){
+        e.target.style.backgroundColor = defaultColor;
+    }
+
+    else if(currentButton == 'bColor') {
+        e.target.style.backgroundColor = chosenColor;
+    }
+
+    else if(currentButton == 'eColor') {
+        e.target.stylebackgroundColor = chosenColor;
     }
 }
+
+
+
+
 
 
 
