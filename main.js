09166299c35d0efac3a4c3235d398f81dc44a643
Arshvir Singh <arshvirsignh16@gmail.com>
2022-08-27 18:54:29 -0600

    Allow user input to color the grid black.

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
+//Temporary Constants
+const tColor = "black";
+
+
+//CONSTANTS
 const bodyWrapper = document.querySelector("#bodyWrapper");
 const gridMain = document.querySelector("#gridMain");
+const gridPieces = document.querySelectorAll("#gridPiece"); 
 
+//GRID
 for(i = 0; i < 16; i++){
-    let gridDiv = document.createElement('div');
-    gridDiv.className = "gridPiece"; 
-    grid.appendChild(gridDiv);
+    let gridPiece = document.createElement('div');
+    gridPiece.className = "gridPiece"; 
+    gridPiece.addEventListener('mouseover', colorGridPiece);
+    gridPiece.addEventListener('mousedown', colorGridPiece);
+    gridMain.appendChild(gridPiece);
 }
+
+
+
+//Drawing Function
+
+function colorGridPiece(e) {
+    if(e.type == 'mouseover' && e.type != 'mousedown') {
+        return;
+    }
+
+    if(tColor == 'black') {
+        e.target.style.backgroundColor = "black";
+    }
+}
+
+
 
 
 
