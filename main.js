cd0cce35e2ad9c0cc566eae977828b4a287bf227
Arshvir Singh <arshvirsignh16@gmail.com>
2022-09-01 17:41:05 -0600

    Make everything functional except click and drag.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let currentButton = 'bColor';
 let defaultSize = 4;
 let newSize;
-let activeButton = '';
+let activeButton = 'color-button';
 let chosenColor = '#ffff';
 let hex = '#ffff';
 
@@ -10,7 +10,8 @@ let hex = '#ffff';
 const bodyWrapper = document.getElementById("bodyWrapper");
 const gridMain = document.getElementById("gridMain");
 const colorIndicator = document.getElementById('color-indicator');
-const colorButton = document.getElementById("pen-color"); 
+const colorButton = document.getElementById("pen-color");
+const rainbowButton = document.getElementById("rainbow"); 
 const eraserButton = document.getElementById("eraser");
 const gridButton = document.getElementById("grid-size");
 const clearButton = document.getElementById("clear");
@@ -20,6 +21,7 @@ const btns = document.querySelectorAll(".button");
 //Variables dependent on constants
 let clrBtn = colorButton.classList;
 let ersrBtn = eraserButton.classList;
+let rainBtn = rainbowButton.classList;
 
 //Color Picker Wheel
 const colorPicker = new iro.ColorPicker("#color-picker", {
@@ -77,6 +79,11 @@ function pickColor() {
     console.log(chosenColor);
 }
 
+rainbowButton.addEventListener('click', rainbowColor);
+function rainbowColor() {
+    currentButton = 'rColor';
+}
+
 eraserButton.addEventListener('click', eraseColor);
 function eraseColor() {
     currentButton = 'eColor';
@@ -93,17 +100,29 @@ function clearAll() {
 //WINDOW ONLOAD
 clrBtn.add("active");
 
+let mousedown = false;
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
+console.log(mousedown);
 
 //Drawing Function
 function colorGridPiece(e) {
-    if(e.type === 'mouseover' && !mousedown) return
+    if(e.type == 'mouseover' && !mousedown) {
+        return;
+    }
 
+    
     if(currentButton === 'bColor') {
         e.target.style.backgroundColor = chosenColor;
     }
 
-    
+    else if(currentButton == 'rColor') {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
 
     else if(currentButton === 'eColor') {
         e.target.style.backgroundColor = chosenColor;
@@ -111,10 +130,7 @@ function colorGridPiece(e) {
 
 }
 
-// This will allow the user to click and drag to draw
-let mousedown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+
 
 //Button Active, Not Active function
 btns.forEach(button => {
@@ -131,6 +147,11 @@ function activateButton(activeButton) {
         deactivateButton(activeButton);
     }
 
+    else if(activeButton == "rainbow-button") {
+        rainBtn.add("active");
+        deactivateButton(activeButton);
+    }
+
     else if(activeButton == "eraser-button") {
         ersrBtn.add("active");
         deactivateButton(activeButton);
@@ -143,11 +164,18 @@ function activateButton(activeButton) {
 
 function deactivateButton(activeButton) {
     if(activeButton == "color-button") {
+        rainBtn.remove("active");
+        ersrBtn.remove("active");
+    }
+
+    else if(activeButton == "rainbow-button") {
+        clrBtn.remove("active");
         ersrBtn.remove("active");
     }
 
     else if(activeButton == "eraser-button") {
         clrBtn.remove("active");
+        rainBtn.remove("active");
     }
 }
 
